539d267aa3b6d5e0582725cde8803749a50d7ac8
Joseph Percival <joseph.m.percivaL@gmail.com>
2021-11-15 15:49:05 +0000

    completed the modal functionalities logic and styling to blur the background and so it is sticky

--- scripts/dynamic-script.js
+++ scripts/dynamic-script.js
@@ -1,38 +1,33 @@
-// Menu Toggle
-const toggleMenu = document.getElementsByClassName('nav-toggle')[0];
-const navBarLinks = document.getElementsByClassName('nav-bar-links')[0];
-
-toggleMenu.addEventListener('click', () => {
-    navBarLinks.classList.toggle('active');
-})
-
-function toggleNav() {
-    document.body.classList.toggle('activeNav');
-}
-
 //   Modal toggle 
 
 var modal = document.querySelector('.modal');
+var blur = document.getElementById('blur');
+function toggleModal(e) {
+  modal.classList.toggle('active');
+  
+//  document.querySelectorAll('.editorial-card').style = "filter:blur(10px);";
+  overlay.classList.toggle('active');
+}
+// function openModal() {
+//   modal.style = "display:flex;";
+// };
 
-function openModal () {
-  modal.style = "display:flex;";
-};
-
-function closeModal () {
-  modal.style = "display:none;";
-};
+// function closeModal() {
+//   modal.style = "display:none;";
+  // overlay.style = "filter:blur(1rem);";
+// };
 
 
 // debugging section 
 // Debug Code to find elements causing horizontal scroll 
 var docWidth = document.documentElement.offsetWidth;
 [].forEach.call(
-document.querySelectorAll('*'),
-function (el) {
-  if (el.offsetWidth > docWidth) {
-    console.log(el);
+  document.querySelectorAll('*'),
+  function (el) {
+    if (el.offsetWidth > docWidth) {
+      console.log(el);
+    }
   }
-}
 );
 // end of debug code
 // });
